bf6b86b8fb8c57ebe7298d7ba8207d0f7a9796dc
Unknown <steven.chou.second@gmail.com>
2017-05-13 15:54:32 +0800

    pug 模板繼承

--- dan-thats-delicious/routes/index.js
+++ dan-thats-delicious/routes/index.js
@@ -5,10 +5,14 @@ const router = express.Router();
 router.get('/', (req, res) => {
   const steven = {name: 'Steven', age: 77, cool: true};
   // res.send('Hey! It works!');
-  res.json(steven);
+  // res.json(steven);
 
   // res.send(req.query.name);
   // res.json(req.query);
+  res.render('hello', {
+    name: 'Steven',
+    dog: req.query.dog
+  });
 });
 
 router.get('/reverse/:name', (req, res) => {
