c8d1f3f0f849da7e7787e73c2c0171b40178982a
Unknown <steven.chou.second@gmail.com>
2017-05-13 15:03:52 +0800

    router 基本測試

--- dan-thats-delicious/routes/index.js
+++ dan-thats-delicious/routes/index.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const steven = {name: 'Steven', age: 77, cool: true};
+  // res.send('Hey! It works!');
+  res.json(steven);
+
+  // res.send(req.query.name);
+  // res.json(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  // res.send(req.params.name);
+
+  const reverse = [... req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
