14078c3bb7d10975bd5073f2dd649446871e3134
Unknown <steven.chou.second@gmail.com>
2017-05-17 23:11:55 +0800

    初探 middleware
    route-specific middleware
    global middleware

--- dan-thats-delicious/controllers/storeController.js
+++ dan-thats-delicious/controllers/storeController.js
@@ -1,5 +1,19 @@
+exports.myMiddleware = (req, res, next) => {
+    req.name = 'Steven';
+    if (req.name === 'Steven') {
+        throw Error("That's a very cool name");
+    }
+
+    res.cookie('name', 'Steven.Chou is cool', {maxAge: 90000});
+    // done the work in this middleware
+    // call next method in router
+    next();
+};
+
+
 exports.homepage = (req, res) => {
     const steven = {name: 'Steven', age: 77, cool: true};
+    console.log(req.name);
     // res.send('Hey! It works!');
     // res.json(steven);
 
--- dan-thats-delicious/routes/index.js
+++ dan-thats-delicious/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const storeController = require('./../controllers/storeController');
 
 // Do work here
-router.get('/', storeController.homepage);
+router.get('/', storeController.myMiddleware, storeController.homepage);
 
 router.get('/reverse/:name', (req, res) => {
   // res.send(req.params.name);
