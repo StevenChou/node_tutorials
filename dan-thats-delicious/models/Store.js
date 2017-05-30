ef8c0230743d3b1050187543428b189114b2315f
Unknown <steven.chou.second@gmail.com>
2017-05-30 18:05:11 +0800

    修改 DB RECORD

--- dan-thats-delicious/models/Store.js
+++ dan-thats-delicious/models/Store.js
@@ -14,7 +14,7 @@ const storeSchema = new mongoose.Schema({
         type: String,
         trim: true
     },
-    tag: [String]
+    tags: [String]
 });
 
 storeSchema.pre('save', function(next) {
--- dan-thats-delicious/routes/index.js
+++ dan-thats-delicious/routes/index.js
@@ -11,6 +11,9 @@ router.get('/stores', catchErrors(storeController.getStores));
 
 router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore));
+router.post('/add/:id', catchErrors(storeController.updateStore));
+
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 router.get('/reverse/:name', (req, res) => {
   // res.send(req.params.name);
